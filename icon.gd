f2a84586a146579c40bc5d6fa6762494c86a4b72
Kiyotaka <xsrkt26@outlook.com>
2026-05-14 11:57:46 +0800

    chore: clean up repository root

--- icon.gd
+++
@@ -1,6 +0,0 @@
-extends Sprite2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _process(delta: float) -> void:
-	position.x += 100 * delta
--- package/version1_0.pck
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bfa01355f3eebf54124597963967e5bc627659b8694c16956d91b74a335f7e8c
-size 14218896
